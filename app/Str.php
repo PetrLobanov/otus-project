5af2666d1843c266d8e006a885e1ae2c40c0122d
Petr Lobanov <petrlobanov@MacBook-Air-Petr-2.local>
2026-06-26 00:07:10 +0500

    v2.3.0

--- app/Str.php
+++ app/Str.php
@@ -2,25 +2,13 @@
 
 namespace App;
 
-use Stringy\Stringy;
-
 class Str
 {
-    public string $str;
-    public function create(string $str) 
-    {
-        $this->str = $str;
-    }
+    public function __construct(public string $str)
+    {}
 
     public function addHello(?string $str = null) 
     {
         return $this->str . $str . 'Hello';
-    }
-
-    public function testPackage()
-    {
-        $str = new Stringy('str');
-
-        echo $str->count();
     }
 }
